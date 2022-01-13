ee9db28cade183dfcb0ca463874ab3f40c0048ec
Chase Covington <mchasecov@gmail.com>
2022-01-13 15:04:46 -0500

    feat: new books in table, not console log

--- application.js
+++ application.js
@@ -1,22 +1,50 @@
 let myLibrary = [];
+const newBookForm = document.forms["new-book-form"];
+const libraryIndexTable = document.getElementById("library-index-table");
 
-function Book(title, author, pageCount, haveRead) {
-  this.title = title;
-  this.author = author;
-  this.pageCount = pageCount;
-  this.haveRead = haveRead;
+class Book {
+  constructor(title, author, pageCount) {
+    this.title = title;
+    this.author = author;
+    this.pageCount = pageCount;
+    this.haveRead = false;
+    addBookToLibrary(this);
+  }
 }
 
-Book.prototype.info = function () {
-  console.log(this.title, this.author, this.pageCount, this.haveRead);
-};
+function newLibraryBook() {
+  let newBookData = new FormData(newBookForm);
+  if (!newBookData.get("bookName")) return false;
+  newBook = new Book(
+    newBookData.get("bookName"),
+    newBookData.get("authName"),
+    newBookData.get("pageCount")
+  );
+}
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  resetTable();
+  populateTableWithBooks();
+}
+function resetTable() {
+  while (libraryIndexTable.rows.length > 0) {
+    libraryIndexTable.deleteRow(0);
+  }
 }
 
-const theHobbit = new Book("The Hobbit", "Tolkein", 295, false);
+function populateTableWithBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let newRow = libraryIndexTable.insertRow(i);
+    newRow.insertCell(0).innerHTML = myLibrary[i].title;
+    newRow.insertCell(1).innerHTML = myLibrary[i].author || "N/A";
+    newRow.insertCell(2).innerHTML = myLibrary[i].pageCount || "N/A";
+    newRow.insertCell(3).innerHTML = myLibrary[i].haveRead
+      ? "Has been read"
+      : "Unread";
+  }
+}
 
-theHobbit.info();
-addBookToLibrary(theHobbit);
-console.log(myLibrary);
+document
+  .getElementById("submit-new-book")
+  .addEventListener("click", newLibraryBook);
